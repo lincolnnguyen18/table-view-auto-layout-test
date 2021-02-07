93cb3330f66b37d84dd306ee36b2d719758cc4de
Lincoln Nguyen <letsmakeaworld@icloud.com>
2021-02-06 19:28:38 -0600

    autolayout for tableview done

--- table view auto layout test/ViewController.swift
+++ table view auto layout test/ViewController.swift
@@ -22,6 +22,9 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TestTableViewCell") as! TestTableViewCell
         cell.testLabel.text = "Row: \(indexPath.row)"
+        if indexPath.row == 0 {
+            cell.testLabel.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac quam id lacus dignissim volutpat. Phasellus vulputate id est ut vestibulum. Phasellus ultricies id erat in blandit. Cras laoreet nunc nisl, eu bibendum nibh euismod at. Morbi bibendum purus nec tortor interdum, eu placerat lacus semper. Nunc pellentesque in dui quis varius. Nulla porttitor sagittis eros et ornare. Donec dolor purus, egestas quis lorem quis, commodo efficitur augue. Donec sit amet rutrum leo. Nam gravida sem quis nunc tincidunt, ac ultricies eros vulputate. Donec ultrices consequat augue in pulvinar. Suspendisse consectetur nibh ligula, in condimentum risus ornare vitae. Nullam nisi tortor, iaculis ut ultricies ac, tempus id dolor. Vivamus ullamcorper elementum diam eget elementum."
+        }
         return cell
     }
 }
